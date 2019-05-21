8fe77c5ba89f1fa640f18d005275cd99540b327e
SacraficeEverything <48491922+SacraficeEverything@users.noreply.github.com>
2019-05-21 01:13:53 -0400

    Update to Main Game
    -Fixed Whack path
    -Fixed output in ridetimes
    -Statistics needs to have 2 paths for each game

--- GameMain.java
+++ GameMain.java
@@ -1,131 +1,3 @@
-import java.lang.Math;
-import java.util.Scanner;
-import java.time.LocalDateTime;
-import java.io.*;
-public class GameMain{
-  
-  public static void main(String[] args) throws IOException{
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Welcome to the game menu! Before we being, please enter your first and last name, separated by an underscore (_).");
-    String name = scan.nextLine();
-    int option = menu();
-    
-    switch (option) {
-      case 1:
-        System.out.println("Monday");
-        break;
-      case 2:
-        int score = rideTimes();
-        System.out.println("Your Final Score for Ride Times is " + score*10 + "%!");
-        System.out.println("Would you like to save this score to a downloadable file on your computer?");
-        saveToFile(score, name);
-        break;
-      case 3:
-        System.out.println("Wednesday");
-        break;
-      case 0:
-        System.out.println("Have a good day!");
-        break;
-    }
-  }
-  
-  public static void saveToFile(int score, String name)  throws IOException{
-    FileWriter fwriter = new FileWriter("Statistics.txt", true);
-    PrintWriter statFile = new PrintWriter(fwriter);
-    PrintWriter outputFile = new PrintWriter(name + "_GameSession_" + getDate() + "_" + getTime() + ".txt");
-    outputFile.println("It works.");
-    outputFile.close();
-  }
-  
-  public static String getTime(){
-    String time = "" + LocalDateTime.now();
-    time = time.substring(time.indexOf("T")+1,time.indexOf("T")+9);
-    for(int i =0; i < time.length()-1;i++)
-    {
-      String beginning = "";
-      
-      if(time.charAt(i)==':'){
-        beginning = time.substring(0,i);
-        time = beginning + "_" + time.substring(i+1);
-      }
-    }
-    return time;
-  }
-  
-  public static String getDate(){
-    String date = "" + LocalDateTime.now();
-    date = date.substring(0, date.indexOf("T"));
-    return date;
-  }
-  public static int menu() { 
-    int option; 
-    Scanner input = new Scanner(System.in); 
-    do { 
-      System.out.println("Your options are:"); 
-      System.out.println("-----------------"); 
-      System.out.println("\t1) Play Wack-a-Mole"); 
-      System.out.println("\t2) Play Ride Times Conversion"); 
-      System.out.println("\t3)Statistics from this session"); 
-      System.out.println("\t0)EXIT");  
-      System.out.print("\nPlease enter a valid mumber: "); 
-      option = input.nextInt(); 
-      System.out.println(); 
-    } while(option < 0 || option > 3); 
-    return option; 
-  }
-  
-  //rideTimes creates a game in which the system spits out a ride time in seconds. The user must then return
-  //the correct minute conversion for that value. This value must be a double, with all digits before the decimal point being minutes
-  //and all after being seconds, not to exceed .6, which would immediately round up to another minute.
-  public static double secToMin(int d){
-    if(d <60)
-      System.out.println("Number too low");
-    double conversion=0;
-    conversion = (((double)((int)(((double)d/60)*100)))/100);
-    
-    return conversion;
-  }
-  public static int rideTimes(){
-    System.out.println("This game tests your division skills. You will be given a number to divide. Good Luck!\n");
-    Scanner keyboard = new Scanner(System.in);
-    double answer = 0.0;
-    int finalScore = 0;
-    int question = 0;
-    for (int i = 0; i <10; i++){
-      question = (int)((Math.random()*((600-60) + 1)) + 60);
-      System.out.println("This ride has a run-time of " + question + " seconds. Please convert it to minutes, rounding to" +
-                         " two decimal places.");
-      
-      answer = getDouble(keyboard,"");
-      if (answer == secToMin(question)){
-        finalScore+=1;
-        System.out.println("Good Job!\n");
-        
-      }else
-        System.out.println("Sorry, but the answer was " + secToMin(question) + "!\n");
-    }
-    return finalScore;
-  }
-  public static double getDouble(Scanner input, String prompt){
-    System.out.print(prompt);
-    while(!input.hasNextDouble()){
-      input.next();
-      System.out.println("Not a double! Try again");
-    }
-    return input.nextDouble();
-  }
-  
-  public static String getString(Scanner input, String prompt){
-    System.out.print(prompt);
-    while(!input.hasNextLine()){
-      input.next();
-      System.out.println("Not a word! Try again");
-    }
-    return input.nextLine();
-    
-  }
-}
-
 /**
  
  *
@@ -158,7 +30,7 @@ public class Menu extends JFrame implements ActionListener{
 	
 	JPanel titleNamePanel, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel, 
 	whackPanel, ridePanel, statPanel, 
-	whackPanel2, ridePanel2, statPanel2,
+	whackPanel2, ridePanel2, statPanel2, whackPanel5, whackPanel4,whackPanel6, whackPanel7, whackPanel8,
 	whackPanel3, ridePanel3, ridePanel4, whackPan, RidePan, statPan,
 	playerPanel2, statPanel3;
 
@@ -180,7 +52,7 @@ public class Menu extends JFrame implements ActionListener{
 	JTextArea mainTextArea, whackText, rideText, statText, statText3 ,
 	whackText2 , rideText2, statText2,
 	whackText3 , rideText3, statText4,statText5,
-	whackText4, whackText5, whackText6;
+	whackText4, whackText5, whackText6 ,whackText7, whackText8 ,whackText9, whackText10, whackTex11;
 	
 	JTextField whackTxt, RideTxt, statTxt;
 
@@ -508,29 +380,15 @@ System.exit(0);
 	
 	public void whackamoleScreen2(){
 	
-		lvlLabelNo.setVisible(false);
-		triesLabelNo.setVisible(false);
-	
-	
-	
-	playerPanel.setVisible(false);
+	lvlLabelNo.setVisible(true);
+	triesLabelNo.setVisible(true);
+	playerPanel.setVisible(true);
 	mainTextPanel.setVisible(false);
 	choiceButtonPanel.setVisible(false);
 	whackPanel.setVisible(false);
 	whackPan.setVisible(false);
 	
-	whackPanel2= new JPanel();
-	whackPanel2.setBounds(100, 60, 600, 400);
-	whackPanel2.setBackground(Color.black);
-	whackPanel2.setVisible(true);
-	con.add(whackPanel2);
-	
-	whackPanel3= new JPanel();
-	whackPanel3.setBounds(100,500,600,200);
-	whackPanel3.setBackground(Color.black);
-	whackPanel3.setVisible(true);
-	con.add(whackPanel3);
-	
+
 	
 	playerPanel2=new JPanel();
 	playerPanel2.setBounds(100,15,600,50);
@@ -547,107 +405,208 @@ System.exit(0);
      int[] choices = RandomizeArray(1, 3);
      int score = 0;
      int option;
-     int answer=0;;
+     int answer=0;
+   
      String dialog;
      
+     while(lives>0) { 
      
-	for (int i = 0; i<3; i++){
-        option = choices[i];
-    switch (option) {
-      case 1:
-    	  whackText2= new JTextArea("" );
-    		whackText2.setBounds(100,300,600,200);
-    		whackText2.setBackground(Color.green);
-    		whackText2.setForeground(Color.white);
-    		whackText2.setFont(gameFont);
-    		whackText2.setLineWrap(true);
-    		whackPanel2.add(whackText2);
-    	  whackText3= new JTextArea ("Please enter the number in the above array which is a multiple of 3");
-    	  whackText3.setBounds(10,10,600,200);
-    	  whackText3.setBackground(Color.white);
-    	  whackText3.setForeground(Color.white);
-    	  whackText3.setFont(normalFont);
-    	  whackText3.setLineWrap(true);
-    	  whackPanel3.add(whackText3);
-    	  
-    	  
-    	  
-          System.out.println(mole1[0] + ", " + mole1[1] + ", " + mole1[2] + ", ");
-          System.out.println(mole1[3] + ", " + mole1[4] + ", " + mole1[5] + ", ");
-          System.out.println(mole1[6] + ", " + mole1[7] + ", " + mole1[8]);
-          System.out.println();
-          System.out.println("The Prime Number is: 3");
-  //        answer = getInt(keyboard, "Please enter the number in the above array which is a multiple of 3\n");
-          String answerr = JOptionPane.showInputDialog("Please Enter you're answer:");
-      	
+     for (int i = 0; i<3; i++){
+         option = choices[i];
+         
+         
+     switch (option) {
+        case 1: 
+        	whackPanel2= new JPanel();
+        	whackPanel2.setBounds(100, 60, 600, 270);
+        	whackPanel2.setBackground(Color.blue);
+        	whackPanel2.setVisible(true);
+        	con.add(whackPanel2);
+        	
+        	whackPanel3= new JPanel();
+        	whackPanel3.setBounds(100,400,600,200);
+        	whackPanel3.setBackground(Color.yellow);
+        	whackPanel3.setVisible(true);
+        	con.add(whackPanel3);
+        	
+    	  whackText4= new JTextArea(mole1[0] + " , " + mole1[1] + ", " + mole1[2] + " \n" 
+    			  					+mole1[3] + " , " + mole1[4] + ", " + mole1[5] + " \n" 
+    			  					+mole1[6] + "  , " + mole1[7] + " , " + mole1[8] );
+    	whackText4.setBounds(100,300,600,200);
+    	whackText4.setBackground(Color.green);
+    	whackText4.setForeground(Color.white);
+    	whackText4.setFont(gameFont);
+    	whackText4.setLineWrap(true);
+    		whackPanel2.add(whackText4);
+    	  whackText5= new JTextArea ("The Prime Number is: 3 \n\n Please enter the number in the above array which is a multiple of 3");
+    	  whackText5.setBounds(100,200,600,200);
+    	  whackText5.setBackground(Color.red);
+    	  whackText5.setForeground(Color.black);
+    	  whackText5.setFont(normalFont);
+    	  whackText5.setLineWrap(true);
+    	  whackPanel3.add(whackText5);
+    
+         String answerr = JOptionPane.showInputDialog("Please Enter you're answer:");
       	 answer = (int) Double.parseDouble(answerr);
+      	
+	   
+	   
           if (answer == 42){
-              System.out.println("Good job!\n");
-          score++;
+        	  JOptionPane.showInternalConfirmDialog(null, "You have entered: "+answer,"Great Job!", JOptionPane.WARNING_MESSAGE);
+        	  score++;
+        	  lvl++;
+        	  whackText4.setText(null);
+        	  whackText5.setText(null);
+        	  whackPanel2.setVisible(false);
+        	  whackPanel3.setVisible(false);
+        	  break;
+          
           }
-          else
-          System.out.println("Sorry but that was the wrong answer!\n");
-          break;
-      case 2:
-    	  whackText2= new JTextArea( );
-    		whackText2.setBounds(100,300,600,200);
-    		whackText2.setBackground(Color.green);
-    		whackText2.setForeground(Color.white);
-    		whackText2.setFont(gameFont);
-    		whackText2.setLineWrap(true);
-    		whackPanel2.add(whackText2);
-    	  whackText3= new JTextArea ("2");
-    	  whackText3.setBounds(10,10,600,200);
-    	  whackText3.setBackground(Color.black);
-    	  whackText3.setForeground(Color.white);
-    	  whackText3.setFont(normalFont);
-    	  whackText3.setLineWrap(true);
-    	  whackPanel3.add(whackText3);
+        
+        	  
+          
+     
+          if (answer >=43 || answer <=42) {
+        	  
+        	  
+        	JOptionPane.showMessageDialog(null, "Sorry, but that was the wrong answer." , ",", JOptionPane.PLAIN_MESSAGE);
+		    lives--;
+		    JOptionPane.showMessageDialog(null, "Lives Left :" +lives, ",", JOptionPane.PLAIN_MESSAGE);
+		    //dialog = JOptionPane.showInputDialog("Try Again:");
+		    //answer = Integer.parseUnsignedInt(dialog);
+		    break;
+          }
+          
+          
+          
+          
+          
+        case 2: 
+        	
+        	whackPanel4= new JPanel();
+        	whackPanel4.setBounds(100, 60, 600, 270);
+        	whackPanel4.setBackground(Color.blue);
+        	whackPanel4.setVisible(true);
+        	con.add(whackPanel4);
+        	
+        	whackPanel5= new JPanel();
+        	whackPanel5.setBounds(100,400,600,200);
+        	whackPanel5.setBackground(Color.blue);
+        	whackPanel5.setVisible(true);
+        	con.add(whackPanel5);
+        	
+        	whackText6= new JTextArea(mole2[0] + " , " + mole2[1] + ", " + mole2[2] + " \n" 
+  					+mole2[3] + " , " + mole2[4] + ", " + mole2[5] + " \n" 
+  					+mole2[6] + "  , " + mole2[7] + " , " + mole2[8] );
+    		whackText6.setBounds(100,300,600,200);
+    		whackText6.setBackground(Color.blue);
+    		whackText6.setForeground(Color.white);
+    		whackText6.setFont(gameFont);
+    		whackText6.setLineWrap(true);
+    		whackPanel4.add(whackText6);
+    	  whackText7= new JTextArea ("The Prime Number is: 7 \n\n Please enter the number in the above array which is a multiple of 3");
+    	  whackText7.setBounds(10,10,600,200);
+    	  whackText7.setBackground(Color.blue);
+    	  whackText7.setForeground(Color.white);
+    	  whackText7.setFont(normalFont);
+    	  whackText7.setLineWrap(true);
+    	  whackPanel5.add(whackText7);
     	  
-          System.out.println(mole2[0] + ", " + mole2[1] + ", " + mole2[2] + ", ");
-          System.out.println(mole2[3] + ", " + mole2[4] + ", " + mole2[5] + ", ");
-          System.out.println(mole2[6] + ", " + mole2[7] + ", " + mole2[8]);
-          System.out.println();
-          System.out.println("The Prime Number is: 7");
-    //      answer = getInt(keyboard, "Please enter the number in the above array which is a multiple of 7\n");
-          if (answer == 28){
-              System.out.println("Good job!\n");
-          score++;
-          }
-          else
-          System.out.println("Sorry but that was the wrong answer!\n");
-          break;
-      case 3:
-    	  whackText2= new JTextArea( );
-    		whackText2.setBounds(100,300,600,200);
-    		whackText2.setBackground(Color.green);
-    		whackText2.setForeground(Color.white);
-    		whackText2.setFont(gameFont);
-    		whackText2.setLineWrap(true);
-    		whackPanel2.add(whackText2);
-    	  whackText3= new JTextArea ("3");
-    	  whackText3.setBounds(10,10,600,200);
-    	  whackText3.setBackground(Color.black);
-    	  whackText3.setForeground(Color.white);
-    	  whackText3.setFont(normalFont);
-    	  whackText3.setLineWrap(true);
-    	  whackPanel3.add(whackText3);
+         
     	  
-          System.out.println(mole3[0] + ", " + mole3[1] + ", " + mole3[2] + ", ");
-          System.out.println(mole3[3] + ", " + mole3[4] + ", " + mole3[5] + ", ");
-          System.out.println(mole3[6] + ", " + mole3[7] + ", " + mole3[8]);
-          System.out.println();
-          System.out.println("The Prime Number is: 6");
-   //       answer = getInt(keyboard, "Please enter the number in the above array which is a multiple of 6\n");
-          if (answer == 18){
-              System.out.println("Good job!\n");
-          score++;
-          }
-          else
-          System.out.println("Sorry but that was the wrong answer!\n");
-          break;
-    }
+    	         String answerrr = JOptionPane.showInputDialog("Please Enter you're answer:");
+    	      	 answer = (int) Double.parseDouble(answerrr);
+    	      	
+    		       
+    	      	
+    		    
+    	          if (answer == 28){
+    	        	  JOptionPane.showInternalConfirmDialog(null, "You have entered: "+answer,"Great Job!", JOptionPane.INFORMATION_MESSAGE);
+    	        	  score++;
+    	        	  lvl++;
+    	        	  whackText6.setText(null);
+    	        	  whackText7.setText(null);
+    	        	  whackPanel4.setVisible(false);
+    	        	  whackPanel5.setVisible(false);
+    	        	break;
+    	        	
+    	        	  
+    	          }
+    	          if (answer >=29 || answer <=27) {
+    	        	  
+    	        	JOptionPane.showMessageDialog(null, "Sorry, but that was the wrong answer." , ",", JOptionPane.PLAIN_MESSAGE);
+    			    lives--;
+    			    JOptionPane.showMessageDialog(null, "Lives Left :" +lives, ",", JOptionPane.PLAIN_MESSAGE);
+    			    //dialog = JOptionPane.showInputDialog("Try Again:");
+    			    //answer = Integer.parseUnsignedInt(dialog);
+     break;
+    	          }
+  
+ 
+     
+	case 3: option = 3;
+	whackPanel6= new JPanel();
+	whackPanel6.setBounds(100, 60, 600, 270);
+	whackPanel6.setBackground(Color.red);
+	whackPanel6.setVisible(true);
+	con.add(whackPanel6);
+	
+	whackPanel7= new JPanel();
+	whackPanel7.setBounds(100,400,600,200);
+	whackPanel7.setBackground(Color.red);
+	whackPanel7.setVisible(true);
+	con.add(whackPanel7);
+	
+	whackText8= new JTextArea(mole3[0] + " , " + mole3[1] + ", " + mole3[2] + " \n" 
+				+mole3[3] + " , " + mole3[4] + ", " + mole3[5] + " \n" 
+				+mole3[6] + "  , " + mole3[7] + " , " + mole3[8] );
+    		whackText8.setBounds(100,300,600,200);
+    		whackText8.setBackground(Color.red);
+    		whackText8.setForeground(Color.white);
+    		whackText8.setFont(gameFont);
+    		whackText8.setLineWrap(true);
+    		whackPanel6.add(whackText8);
+    	  whackText9= new JTextArea ("The Prime Number is: 6 \n\n Please enter the number in the above array which is a multiple of 6");
+    	  whackText9.setBounds(10,10,600,200);
+    	  whackText9.setBackground(Color.red);
+    	  whackText9.setForeground(Color.white);
+    	  whackText9.setFont(normalFont);
+    	  whackText9.setLineWrap(true);
+    	  whackPanel7.add(whackText9);
+    	  
+    	  
+    	         String answerrrr = JOptionPane.showInputDialog("Please Enter you're answer:");
+    	      	 answer = (int) Double.parseDouble(answerrrr);
+    	      	
+    		       
+    		   
+    	          if (answer == 18){
+    	        	  JOptionPane.showInternalConfirmDialog(null, "You have entered: "+answer,"Great Job!", JOptionPane.INFORMATION_MESSAGE);
+    	        	  score++;
+    	        	  lvl++;
+    	        	  whackText8.setText(null);
+    	        	  whackText9.setText(null);
+    	        	  whackPanel6.setVisible(false);
+    	        	  whackPanel7.setVisible(false);
+    	        	  break;
+    	        	 
+    	        	  
+    	          }
+    	          if (answer >=19 || answer <=17) {
+    	        	  
+    	        	JOptionPane.showMessageDialog(null, "Sorry, but that was the wrong answer." , ",", JOptionPane.PLAIN_MESSAGE);
+    			    lives--;
+    			    JOptionPane.showMessageDialog(null, "Lives Left :" +lives, ",", JOptionPane.PLAIN_MESSAGE);
+    			    //dialog = JOptionPane.showInputDialog("Try Again:");
+    			    //answer = Integer.parseUnsignedInt(dialog);
+    		   
+    	  break;
+      
+    	          }
+    }    
 	}
+     }
+     }
 	/*
 	
 	for (int i = 0; i<3; i++){
@@ -821,7 +780,7 @@ System.exit(0);
 	        
 	        JOptionPane.showMessageDialog(null, "Lives Left :" +lives, ",", JOptionPane.PLAIN_MESSAGE);
 	        */
-	}
+	
 		
 	
 	public void rideTimesScreen() {
@@ -1324,6 +1283,10 @@ case "exitR": System.exit(0);
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void reset() {
+		
+	}
 
 
 public static double secToMin(int d){
@@ -1385,7 +1348,11 @@ public String getDate(){
       beginning = date.substring(0,i);
       date = beginning + "/" + date.substring(i+1);
     }
+    
+    
 }
+  
+
 return date;
 }}
 	
